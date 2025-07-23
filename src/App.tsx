2549b5c06bb263600d54461a5993c8bcd600c206
Juan <jcsantamar@gmail.com>
2025-07-23 20:40:16 +0200

    Tailwind v3 instalado

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,49 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <Router>
+      <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
-      <div className="bg-blue-500 text-white p-4">
+
+      <div className="p-4 text-white bg-blue-500">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img
             src={reactLogo}
-            className="logo react bg-white"
+            className="bg-white logo react"
             alt="React logo"
           />
         </a>
+
+      <div className="flex items-center justify-center min-h-screen bg-blue-200">
+        <h1 className="text-4xl font-bold text-blue-900">
+          Vite + React (y Tailwind is working!)
+        </h1>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div> */}
-      <div className="min-h-screen bg-blue-200 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-900">
-          Tailwind is working!
-        </h1>
       </div>
+      </div>
+
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
